65256feb680d37a9b1d029fe747c68aa29181ff2
mgoodrich <mark.goodrich@live.com>
2018-06-01 14:15:12 -0400

    add new authenticated router provided by react-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import createStore from './store';
-import LoginPage from './components/pages/LoginPage';
+import { AuthenticatedRoute, LoginPage } from '@openmrs/react-components';
+import HomePage from './components/pages/HomePage';
 
 const store = createStore();
 
@@ -13,7 +14,10 @@ class App extends Component {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Route path="/" component={LoginPage}/>
+          <Switch>
+            <Route path="/login" component={LoginPage}/>
+            <AuthenticatedRoute path="/" component={HomePage}/>
+          </Switch>
         </BrowserRouter>
       </Provider>
     );
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = props => {
+  return (
+    <div className="App">
+      <b>Hello World</b>
+    </div>
+  );
+};
+
+export default HomePage;
