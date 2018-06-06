0565576e4ba5b31c7d80f950e69a7e7e044f3847
Cosmin Ioan <cioan@pih.org>
2018-06-06 16:20:54 -0400

    display search results

--- src/components/pages/SearchPatient.jsx
+++ src/components/pages/SearchPatient.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import { PatientSearch } from '@openmrs/react-components';
 
 const SearchPatient = props => {
+
+    this.state =  {
+        columnDefs: [
+            {headerName: 'patientUuid', hide: true, valueGetter: 'data.uuid' },
+            {headerName: 'ID', valueGetter: 'data.identifier' },
+            {headerName: 'First Name', valueGetter: 'data.firstName' },
+            {headerName: 'First Name', valueGetter: 'data.firstName' },
+            {headerName: 'Last Name', valueGetter: 'data.lastName' },
+            {headerName: 'Gender', field: 'gender' },
+            {headerName: 'Age', field: 'age' }
+        ]
+    };
+
     return(
-        <PatientSearch />
+        <PatientSearch columnDefs={this.state.columnDefs}/>
     )
-}
+};
 
 export default SearchPatient;
