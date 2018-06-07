c763a711326e199b1dcf783c644f4746443588bd
Cosmin Ioan <cioan@pih.org>
2018-06-07 14:35:55 -0400

    pass callback function to parse results

--- src/components/pages/SearchPatient.jsx
+++ src/components/pages/SearchPatient.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import { PatientSearch } from '@openmrs/react-components';
 
-const SearchPatient = props => {
+class SearchPatient extends React.Component {
 
-    const columnDefs =  [
+    constructor(props) {
+        super(props);
+        this.columnDefs =  [
             {headerName: 'patientUuid', hide: true, valueGetter: 'data.uuid' },
             {headerName: 'ID', valueGetter: 'data.identifier' },
-            {headerName: 'First Name', valueGetter: 'data.firstName' },
             {headerName: 'First Name', valueGetter: 'data.firstName' },
             {headerName: 'Last Name', valueGetter: 'data.lastName' },
             {headerName: 'Gender', field: 'gender' },
             {headerName: 'Age', field: 'age' }
         ];
+    }
 
-    return(
-        <PatientSearch columnDefs={ columnDefs }/>
-    )
+
+    parseResults(results) {
+        console.log("parseResults callback function, length =  " + results.length);
+        let patients = [];
+        for (const item of results) {
+            let patient = {
+                uuid: item.uuid,
+                id: item.id,
+                firstName: item.person.names[0].givenName,
+                lastName: item.person.names[0].familyName,
+                gender: item.person.gender,
+                age: item.person.age,
+                identifier: item.identifiers[0].identifier,
+                checkedInTime: null,
+                birthdate: item.person.birthdate
+            };
+            patients.push(patient);
+        }
+        return patients;
+    };
+
+    render() {
+        return(
+            <PatientSearch columnDefs={ this.columnDefs } parseResults={ this.parseResults.bind(this) }/>
+        );
+    }
 };
 
 export default SearchPatient;
