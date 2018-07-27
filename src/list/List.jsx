68e5a580149d8d4f792f7fe6a8e078d6d28084f2
mgoodrich <mark.goodrich@live.com>
2018-07-27 15:54:57 -0400

    refactor and generized List in preparation for moving to openmrs-reactcomponents

--- src/list/List.jsx
+++ src/list/List.jsx
@@ -1,54 +1,48 @@
 import React from 'react';
 import { push } from 'connected-react-router';
 import { Label } from 'react-bootstrap';
-import { visitActions } from '@openmrs/react-components';
 import { DataGrid } from '@openmrs/react-components';
-import patientActions from '../patient/patientActions';
-import { PATIENT_REPRESENTATION, ENCOUNTER_REPRESENTATION } from '../constants';
-import utils from "../utils";
 
 class List extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    // TOODO: allow this to be passed in as a prop
-
-    this.columnDefs =  [
-      { headerName: 'uuid', hide: true, field: 'uuid' },
-      { headerName: 'Id', valueGetter: 'data.identifiers[0].identifier' },
-      { headerName: 'patientId', field: 'id' },
-      { headerName: 'Given Name', field: 'name.givenName' },
-      { headerName: 'Family Name', field: 'name.familyName' },
-      { headerName: 'Gender', field: 'gender' },
-      { headerName: 'Age', field: 'age' },
-      {
-        headerName: 'Checked-in Time', valueGetter: function getCheckedInTime(params) {
-          return utils.getPatientCheckedInTime(params.data);
-        }
-      }
-    ];
-  }
-
   componentDidMount() {
-    this.props.dispatch(patientActions.clearPatientSelected());
-    // TODO make this potentially come from props so we can override it?
-    // TODO can we get away from having to get a "full" rep of a patient?
-    this.props.dispatch(this.fetchListAction());
-    this.interval = setInterval(() =>
-      this.props.dispatch(this.fetchListAction()), 10000);
+
+    if (this.fetchListAction() !== undefined) {
+      this.props.dispatch(this.fetchListAction());
+      this.interval = setInterval(() =>
+        this.props.dispatch(this.fetchListAction()), 10000);
+    }
+
+    if (this.fetchOtherActions() !== undefined) {
+      this.fetchOtherActions().forEach((action) => this.props.dispatch(action));
+    }
+
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
+  columnDefs() {
+    return [
+      { headerName: 'uuid', hide: true, field: 'uuid' },
+      { headerName: 'Given Name', field: 'name.givenName' },
+      { headerName: 'Family Name', field: 'name.familyName' },
+      { headerName: 'Gender', field: 'gender' },
+      { headerName: 'Age', field: 'age' }
+    ];
+  }
+
   fetchListAction() {
-    return visitActions.fetchActiveVisits("custom:(uuid,patient:" + PATIENT_REPRESENTATION + ",encounters:" + ENCOUNTER_REPRESENTATION + ")");
+    return undefined;  // needs to be overwritten in implementing classes
+  }
+
+  fetchOtherActions() {
+    return undefined;
   }
 
   redirectToInfoPageActionCreator() {
-    return push('/'); // needs to be overwritten in implementing methods
+    return push('/'); // needs to be overwritten in implementing classed
   }
 
   title() {
@@ -60,7 +54,7 @@ class List extends React.Component {
       <div>
         <h3><Label>{this.title()}</Label></h3>
         <DataGrid
-          columnDefs={this.columnDefs}
+          columnDefs={this.columnDefs()}
           rowData={this.props.rowData}
           rowSelectedActionCreators={[this.redirectToInfoPageActionCreator]}
         />
--- src/screening/bloodPressure/BloodPressureQueue.jsx
+++ src/screening/bloodPressure/BloodPressureQueue.jsx
@@ -1,8 +1,37 @@
 import List from '../../list/List';
 import { connect } from "react-redux";
 import { push } from 'connected-react-router';
+import { visitActions } from '@openmrs/react-components';
+import patientActions from '../../patient/patientActions';
+import utils from "../../utils";
+import { PATIENT_REPRESENTATION, ENCOUNTER_REPRESENTATION } from '../../constants';
 
 class BloodPressureQueue extends List {
+
+  columnDefs() {
+    return [
+      { headerName: 'uuid', hide: true, field: 'uuid' },
+      { headerName: 'Id', valueGetter: 'data.identifiers[0].identifier' },  // TODO needs to be replaced with actual preferred identifier
+      { headerName: 'patientId', field: 'id' },
+      { headerName: 'Given Name', field: 'name.givenName' },
+      { headerName: 'Family Name', field: 'name.familyName' },
+      { headerName: 'Gender', field: 'gender' },
+      { headerName: 'Age', field: 'age' },
+      { headerName: 'Checked-in Time',
+        valueGetter: function getCheckedInTime(params) {
+          return utils.getPatientCheckedInTime(params.data);
+        }
+      }
+    ];
+  }
+
+  fetchListAction() {
+    return visitActions.fetchActiveVisits("custom:(uuid,patient:" + PATIENT_REPRESENTATION + ",encounters:" + ENCOUNTER_REPRESENTATION + ")");
+  }
+
+  fetchOtherActions() {
+    return [ patientActions.clearPatientSelected() ];
+  }
 
   redirectToInfoPageActionCreator() {
     return push('/screening/bloodPressure/form');
