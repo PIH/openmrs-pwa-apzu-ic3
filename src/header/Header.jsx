2de45c8ab59b27612920ef536c2ce3a4a681d9f0
shao yuan <shaoyuan_chewchia@college.harvard.edu>
2018-08-20 17:18:05 -0400

    MLW-738 WIP experimenting with react-bootstrap navbar

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,20 +1,88 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import Menu from './Menu';
-import PatientHeader from '../patient/PatientHeader';
+import { connect } from 'react-redux';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import '../assets/css/header.css';
+import logo from "../assets/images/pih_apzu_logo_white.png";
+import { sessionActions, headerActions, loginActions } from '@openmrs/react-components';
 
-class Header extends React.Component {
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userDropdown: false,
+      locationDropdown: false,
+    };
+  }
+
+  componentDidMount() {
+    this.props.dispatch(sessionActions.fetchSession());
+    this.props.dispatch(loginActions.getLoginLocations());
+    this.props.dispatch(headerActions.getHeaderLogoLinks());
+  }
+
+  toggleState = (key, value) => {
+    this.setState(() => ({
+      [key]: value || !this.state[key],
+    }));
+  }
 
   render() {
+
     return (
-      <div>
-        <Navbar>
-          <PatientHeader />
-          <Menu />
-        </Navbar>
-      </div>
+      <Navbar
+        className="header"
+        fixedTop={false}
+      >
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">
+              <img className="logo"
+                   alt=""
+                   src={logo}
+              />
+            </a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavDropdown eventKey={1} title="Dropdown" id="dropdown">
+          </NavDropdown>
+          <NavDropdown eventKey={3} title={this.props.user ? this.props.user.display : null} className="dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  const { sessionLocation, user } = state.openmrs.session;
+  const { list } = state.openmrs.loginLocations;
+
+  return {
+    sessionLocation,
+    user,
+    locations: list,
+  };
+};
+
+Header.propTypes = {
+  locations: PropTypes.array.isRequired,
+  sessionLocation: PropTypes.shape({ display: PropTypes.string }),
+  user: PropTypes.shape({ display: PropTypes.string })
+};
+
+Header.defaultProps = {
+  locations: [],
+  sessionLocation: {
+    display: '',
+  },
+  user: { display: '' },
+};
+
+export default connect(mapStateToProps)(Header);
