1ddb295b04672e561f745a942baf552540d29eab
Cosmin Ioan <cioan@pih.org>
2018-11-02 16:01:57 -0400

    MLW-824, add in-progress and completed Nutrition queues

--- src/screening/nutrition/nutritionFilters.js
+++ src/screening/nutrition/nutritionFilters.js
@@ -2,5 +2,6 @@ import { patientObjByEncounterTypeFilter  } from "@openmrs/react-components";
 import { ENCOUNTER_TYPES } from "../../constants";
 
 export default {
+  required: patient => patient,
   completed: patientObjByEncounterTypeFilter(ENCOUNTER_TYPES.NutritionEncounterType.uuid, 'include')
 };
