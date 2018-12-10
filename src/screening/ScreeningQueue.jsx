67fefa50d6770c0b1074b1c71fc3083d6c8f3a64
jomadoye <omadoyejed@gmail.com>
2018-12-10 05:02:13 +0100

    Fix whitespace in patient header, make CardList more re-usable

--- src/screening/ScreeningQueue.jsx
+++ src/screening/ScreeningQueue.jsx
@@ -8,6 +8,7 @@ import {
 } from '@openmrs/react-components';
 import {ENCOUNTER_TYPES} from '../constants';
 import { connect } from "react-redux";
+import moment from 'moment';
 import { PATIENT_IDENTIFIER_FILTERS } from "../gridConstants";
 import utils from "../utils";
 import ic3PatientActions from "../patient/patientActions";
@@ -28,13 +29,37 @@ let ScreeningQueue = props => {
       () => props.dispatch(patientActions.clearSelectedPatient())
     ];
 
+  const patientCard = (patient, index, onRowSelected) => (
+    <div 
+      className="card-list"
+      key={index} 
+      onClick={() => onRowSelected(patient)}>
+      <div className="left-items">
+        <span className="name">
+          <span className="given-name">{patient.name && patient.name.givenName && patient.name.givenName}</span>
+          <span className="family-name">{patient.name && patient.name.familyName && patient.name.familyName}</span>
+        </span>
+        <span className="gender-age">
+          <span className="gender">{patient.gender && patient.gender === 'M' ? "Male" : "Female"}</span>
+          <span className="age">{patient.age && patient.age} yrs old</span>
+          <span className="dob">({patient.birthdate && moment(patient.birthdate).format('DD, MMM, YYYY')})</span>
+        </span>
+      </div>
+      <div className="right-items">
+        {utils.getPatientIdentifiers(patient) && utils.getPatientIdentifiers(patient).split('<br/>').map((identifier, index) => (
+          <span key={index}>{identifier}</span>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
     <div>
       <CardList
         fetchListActionCreator={fetchListActionCreator}
         filters={[...props.filters, patientObjByEncounterTypeFilter(ENCOUNTER_TYPES.CheckInEncounterType.uuid, 'include')]}
         loading={props.updating}
-        getIdentifiers={utils.getPatientIdentifiers}
+        card={patientCard}
         onMountOtherActionCreators={onMountOtherActionCreators}
         optionalFilters={ PATIENT_IDENTIFIER_FILTERS }
         optionalFiltersType='or'
