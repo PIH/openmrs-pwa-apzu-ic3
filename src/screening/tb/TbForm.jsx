c77948c2cd2f0e07e9ac9a66339a56fe3a583813
mgoodrich <mark.goodrich@live.com>
2019-01-04 17:45:35 -0500

    fix TB form so that you can't have both "Present" and "Absent" selected for the same symptom

--- src/screening/tb/TbForm.jsx
+++ src/screening/tb/TbForm.jsx
@@ -1,13 +1,84 @@
 /* eslint-disable */
 
 import React from 'react';
-import {Obs} from '@openmrs/react-components';
+import {connect} from 'react-redux';
+import {formUtil, Obs} from '@openmrs/react-components';
+import {change, formValueSelector, untouch} from "redux-form";
 import { Grid, Row, FormGroup, ControlLabel, Col } from 'react-bootstrap';
 import { ENCOUNTER_TYPES, CONCEPTS, FORM_ANSWERS } from "../../constants";
 import ScreeningForm from "../ScreeningForm";
 import { padding, setFlex } from "../../pwaStyles";
 
-export default class TbForm extends React.Component {
+
+class TbForm extends React.Component {
+
+  componentDidUpdate(prevProps) {
+
+    // make sure that we can't have both present and absent checked at the same time
+    // ideally, the form entry technology would have some built in support for this
+    if (this.props.coughPresent !== prevProps.coughPresent && this.props.coughPresent === CONCEPTS.TB.Cough.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-cough-absent', CONCEPTS.SymptomAbsent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-cough-absent', CONCEPTS.SymptomAbsent.uuid)));
+    }
+
+    if (this.props.coughAbsent !== prevProps.coughAbsent && this.props.coughAbsent === CONCEPTS.TB.Cough.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-cough-present', CONCEPTS.SymptomPresent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-cough-present', CONCEPTS.SymptomPresent.uuid)));
+    }
+
+    if (this.props.feverPresent !== prevProps.feverPresent && this.props.feverPresent === CONCEPTS.TB.Fever.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-fever-absent', CONCEPTS.SymptomAbsent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-fever-absent', CONCEPTS.SymptomAbsent.uuid)));
+    }
+
+    if (this.props.feverAbsent !== prevProps.feverAbsent && this.props.feverAbsent === CONCEPTS.TB.Fever.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-fever-present', CONCEPTS.SymptomPresent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-fever-present', CONCEPTS.SymptomPresent.uuid)));
+    }
+
+    if (this.props.nightSweatPresent !== prevProps.nightSweatPresent && this.props.nightSweatPresent === CONCEPTS.TB.NightSweats.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-night-sweat-absent', CONCEPTS.SymptomAbsent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-night-sweat-absent', CONCEPTS.SymptomAbsent.uuid)));
+    }
+
+    if (this.props.nightSweatAbsent !== prevProps.nightSweatAbsent && this.props.nightSweatAbsent === CONCEPTS.TB.NightSweats.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-night-sweat-present', CONCEPTS.SymptomPresent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-night-sweat-present', CONCEPTS.SymptomPresent.uuid)));
+    }
+
+    if (this.props.weightLossPresent !== prevProps.weightLossPresent && this.props.weightLossPresent === CONCEPTS.TB.WeightLoss.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-weight-loss-absent', CONCEPTS.SymptomAbsent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-weight-loss-absent', CONCEPTS.SymptomAbsent.uuid)));
+    }
+
+    if (this.props.weightLossAbsent !== prevProps.weightLossAbsent && this.props.weightLossAbsent === CONCEPTS.TB.WeightLoss.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-weight-loss-present', CONCEPTS.SymptomPresent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-weight-loss-present', CONCEPTS.SymptomPresent.uuid)));
+    }
+
+    if (this.props.recentContactWithActiveTBPresent !== prevProps.recentContactWithActiveTBPresent && this.props.recentContactWithActiveTBPresent === CONCEPTS.TB.RecentContactWithActiveTB.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-recent-contact-with-active-TB-absent', CONCEPTS.SymptomAbsent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-recent-contact-with-active-TB-absent', CONCEPTS.SymptomAbsent.uuid)));
+    }
+
+    if (this.props.recentContactWithActiveTBAbsent !== prevProps.recentContactWithActiveTBAbsent && this.props.recentContactWithActiveTBAbsent === CONCEPTS.TB.RecentContactWithActiveTB.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-recent-contact-with-active-TB-present', CONCEPTS.SymptomPresent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-recent-contact-with-active-TB-present', CONCEPTS.SymptomPresent.uuid)));
+    }
+
+    if (this.props.painfulNeckAndArmpitLymphTBPresent !== prevProps.painfulNeckAndArmpitLymphTBPresent && this.props.painfulNeckAndArmpitLymphTBPresent === CONCEPTS.TB.PainfulNeckAndArmpitLymphNodes.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-painful-neck-and-armpit-lymph-node-absent', CONCEPTS.SymptomAbsent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-painful-neck-and-armpit-lymph-node-absent', CONCEPTS.SymptomAbsent.uuid)));
+    }
+
+    if (this.props.painfulNeckAndArmpitLymphTBAbsent !== prevProps.painfulNeckAndArmpitLymphTBAbsent && this.props.painfulNeckAndArmpitLymphTBAbsent === CONCEPTS.TB.PainfulNeckAndArmpitLymphNodes.uuid) {
+      this.props.dispatch(change(this.props.formInstanceId, formUtil.obsFieldName('tb-painful-neck-and-armpit-lymph-node-present', CONCEPTS.SymptomPresent.uuid), null));
+      this.props.dispatch(untouch(this.props.formInstanceId, formUtil.obsFieldName('tb-painful-neck-and-armpit-lymph-node-present', CONCEPTS.SymptomPresent.uuid)));
+    }
+
+  }
+
+
  render() {
     const formContent = (
       <Grid>
@@ -170,3 +241,36 @@ export default class TbForm extends React.Component {
   }
 
 }
+
+
+export default connect((state, props) => {
+  const selector = formValueSelector(props.formInstanceId);
+
+  const coughPresent = selector(state, formUtil.obsFieldName('tb-cough-present', CONCEPTS.SymptomPresent.uuid));
+  const coughAbsent = selector(state, formUtil.obsFieldName('tb-cough-absent', CONCEPTS.SymptomAbsent.uuid));
+  const feverPresent = selector(state, formUtil.obsFieldName('tb-fever-present', CONCEPTS.SymptomPresent.uuid));
+  const feverAbsent = selector(state, formUtil.obsFieldName('tb-fever-absent', CONCEPTS.SymptomAbsent.uuid));
+  const nightSweatPresent = selector(state, formUtil.obsFieldName('tb-night-sweat-present', CONCEPTS.SymptomPresent.uuid));
+  const nightSweatAbsent = selector(state, formUtil.obsFieldName('tb-night-sweat-absent', CONCEPTS.SymptomAbsent.uuid));
+  const weightLossPresent = selector(state, formUtil.obsFieldName('tb-weight-loss-present', CONCEPTS.SymptomPresent.uuid));
+  const weightLossAbsent = selector(state, formUtil.obsFieldName('tb-weight-loss-absent', CONCEPTS.SymptomAbsent.uuid));
+  const recentContactWithActiveTBPresent = selector(state, formUtil.obsFieldName('tb-recent-contact-with-active-TB-present', CONCEPTS.SymptomPresent.uuid));
+  const recentContactWithActiveTBAbsent = selector(state, formUtil.obsFieldName('tb-recent-contact-with-active-TB-absent', CONCEPTS.SymptomAbsent.uuid));
+  const painfulNeckAndArmpitLymphTBPresent = selector(state, formUtil.obsFieldName('tb-painful-neck-and-armpit-lymph-node-present', CONCEPTS.SymptomPresent.uuid));
+  const painfulNeckAndArmpitLymphTBAbsent = selector(state, formUtil.obsFieldName('tb-painful-neck-and-armpit-lymph-node-absent', CONCEPTS.SymptomAbsent.uuid));
+
+  return {
+    coughPresent,
+    coughAbsent,
+    feverPresent,
+    feverAbsent,
+    nightSweatPresent,
+    nightSweatAbsent,
+    weightLossPresent,
+    weightLossAbsent,
+    recentContactWithActiveTBPresent,
+    recentContactWithActiveTBAbsent,
+    painfulNeckAndArmpitLymphTBPresent,
+    painfulNeckAndArmpitLymphTBAbsent
+  };
+})(TbForm);
