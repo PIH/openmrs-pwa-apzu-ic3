7a88f53f49ce93522ed3b26c9998a4420a1746cf
jomadoye <omadoyejed@gmail.com>
2019-01-07 03:40:11 +0100

    Update PR

--- src/screening/bloodPressure/BloodPressureForm.jsx
+++ src/screening/bloodPressure/BloodPressureForm.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import {Obs, formValidations} from '@openmrs/react-components';
+import { Obs } from '@openmrs/react-components';
 import { Grid, FormGroup, ControlLabel, Col } from 'react-bootstrap';
 import { ENCOUNTER_TYPES, CONCEPTS } from "../../constants";
 import { noPaddingLeftAndRight, flexBaseline } from "../../pwaStyles";
 import ScreeningForm from "../ScreeningForm";
-
-/**
- * Range of possible values
- * SBP 50-260
- * DBP 40-140
- *
- * Abnormal results
- * SBP > 160
- * DBP > 110
- */
-const systolicBloodPressureNormalRange = formValidations.getNormalRangeFromConcept(CONCEPTS.SystolicBloodPressure);
-const systolicBloodPressureAbNormalRange = formValidations.getAbNormalRangeFromConcept(CONCEPTS.SystolicBloodPressure);
-const DiastolicBloodPressureNormalRange = formValidations.getNormalRangeFromConcept(CONCEPTS.DiastolicBloodPressure);
-const DiastolicBloodPressureAbNormalRange = formValidations.getAbNormalRangeFromConcept(CONCEPTS.DiastolicBloodPressure);
 
 let BloodPressureForm = props => {
 
@@ -32,11 +18,9 @@ let BloodPressureForm = props => {
         <FormGroup controlId="formSystolic" style={flexBaseline}>
           <Col sm={2}>
             <Obs
-              concept={CONCEPTS.SystolicBloodPressure.uuid}
+              concept={CONCEPTS.SystolicBloodPressure}
               placeholder="value"
               path="systolic"
-              validate={props.validate || systolicBloodPressureNormalRange}
-              warn={props.warn || systolicBloodPressureAbNormalRange}
             />
           </Col>
           <ControlLabel sm={1} style={noPaddingLeftAndRight}>
@@ -53,11 +37,9 @@ let BloodPressureForm = props => {
         <FormGroup controlId="formDiastolic" style={flexBaseline}>
           <Col sm={2}>
             <Obs
-              concept={CONCEPTS.DiastolicBloodPressure.uuid}
+              concept={CONCEPTS.DiastolicBloodPressure}
               placeholder="value"
               path="diastolic"
-              validate={props.validate || DiastolicBloodPressureNormalRange}
-              warn={props.warn || DiastolicBloodPressureAbNormalRange}
             />
           </Col>
           <ControlLabel sm={1} style={noPaddingLeftAndRight}>
