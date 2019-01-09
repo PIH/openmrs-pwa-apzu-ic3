7266130e7ddf888acad65c437ad8c4cb405bb931
mgoodrich <mark.goodrich@live.com>
2019-01-09 16:06:35 -0500

    Revert "Add chronic care diagnosis"
    This reverts commit f9448b53

--- src/screening/clinician/ClinicianSummary.jsx
+++ src/screening/clinician/ClinicianSummary.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import {ObsHistory, ProgramEnrollment} from '@openmrs/react-components';
-import {CONCEPTS} from "../../constants";
+import {ProgramEnrollment} from '@openmrs/react-components';
 
 const ClinicianSummary = props => {
   return (
     <div>
       <ProgramEnrollment />
       <h4><u>Diagnoses</u></h4>
-      <ObsHistory
-        concepts={[CONCEPTS.ChronicCareDiagnosis]}
-      />
+      <p>Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s</p>
       <br />
       <h4><u>Alert this visit</u></h4>
       <p>Lorem Ipsum is simply dummy text of the printing and 
