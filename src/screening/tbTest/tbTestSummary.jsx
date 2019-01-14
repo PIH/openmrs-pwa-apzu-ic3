226c1b4618bc980e3d6f50298f84197d6cf49203
rowland henshaw <rowland.henshaw@gmail.com>
2019-01-14 10:12:54 +0100

    created initial files for TBtest queue

---
+++ src/screening/tbTest/tbTestSummary.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import {CONCEPTS} from "../../constants";
+// import {ObsHistory} from "@openmrs/react-components";
+
+// const TbSummary = props => {
+//   return (
+//     <ObsHistory
+//       concepts={[CONCEPTS.SymptomPresent,
+//         CONCEPTS.SymptomAbsent]}
+//     />
+//   );
+// };
+
+// export default TBTestSummary;
