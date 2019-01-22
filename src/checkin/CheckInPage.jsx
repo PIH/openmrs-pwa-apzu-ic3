0f1289e4f44fdc9931998f20f0fe0697ac0f2c97
jomadoye <omadoyejed@gmail.com>
2019-01-22 09:28:11 +0100

    fix add new bug in checkin

--- src/checkin/CheckInPage.jsx
+++ src/checkin/CheckInPage.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectors } from '@openmrs/react-components';
 import CheckInSummary from "./CheckInSummary";
 import ReferralForm from "./ReferralForm";
 import SummaryAndForm from "../layout/SummaryAndForm";
+import checkInFilters from "./checkInFilters";
 
-class CheckInPage extends React.Component {
+const CheckInPage = (props) =>  {
+  return (
+    <div>
+      <SummaryAndForm
+        backLink="/checkin/checkInTabs"
+        completed={checkInFilters.completed(props.patient)}
+        form={<ReferralForm />}
+        requireVisitForForm={false}
+        summary={<CheckInSummary />}
+        title="Patient Check-In"
+      />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <SummaryAndForm
-          backLink="/checkin/checkInTabs"
-          form={<ReferralForm/>}
-          requireVisitForForm={false}
-          summary={<CheckInSummary/>}
-          title="Patient Check-In"
-        />
-      </div>
-    );
-  }
-}
+const mapStateToProps = (state) => {
+  return {
+    patient: selectors.getSelectedPatientFromStore(state),
+  };
+};
 
-export default CheckInPage;
+export default connect(mapStateToProps)(CheckInPage);
