6c5a2d2974e9facc1400d511fd44e8fbc7d579ac
mgoodrich <mark.goodrich@live.com>
2019-01-30 14:41:56 -0500

    fix HtcSummary to only show Lab Test summary; tweak display of PatientLabTests component

--- src/patient/PatientLabTests.jsx
+++ src/patient/PatientLabTests.jsx
@@ -11,32 +11,28 @@ class PatientLabTests extends React.Component {
   render() {
     let labTests = null;
     if (typeof this.props.test_type !== 'undefined') {
-      let filteredTests = this.props.patient.labTests[this.props.test_type];
-
-    if ( (typeof filteredTests !== 'undefined') && (filteredTests !== null) ) {
-      filteredTests.sort(function(a,b) {
-        return +new Date(b.specimenDate) - +new Date(a.specimenDate);
-      });
-      labTests = filteredTests.map((lab, i) => {
-        return (
-          <div key={lab.specimenDate}>
-            <h4>{ lab.testType ? (utils.getConceptNameByUuid(lab.testType) + " @ ") : "" } {lab.specimenDate !== null ? utils.formatCalendarDate(lab.specimenDate) : '_'}</h4>
-            <ul>
-              <li>Date: {lab.effectiveDate !== null ? utils.formatCalendarDate(lab.effectiveDate) : utils.formatCalendarDate(lab.specimenDate)}</li>
-              <li>Results: <Label
-                bsStyle="danger"> {([
+      if ((typeof this.props.patient.labTests[this.props.test_type] !== 'undefined')
+        && (this.props.patient.labTests[this.props.test_type] !== null)) {
+        labTests = [...this.props.patient.labTests[this.props.test_type]].reverse().map((lab, i) => {
+          return (
+            <div key={lab.effectiveDate}>
+              <h4>{lab.testType ? (utils.getConceptNameByUuid(lab.testType)) : ""}</h4>
+              <ul>
+                <li>Effective Date: {lab.effectiveDate !== null ? utils.formatCalendarDate(lab.effectiveDate) : ""}</li>
+                <li>Results: <Label
+                  bsStyle="danger"> {([
                   lab.result ? utils.getConceptNameByUuid(lab.result) : "",
                   lab.resultNumeric,
                   lab.resultLdl === true ? "LDL" : lab.resultLdl
-              ]).filter(Boolean).join(", ")}</Label>
-              </li>
-            </ul>
-          </div>
+                ]).filter(Boolean).join(", ")}</Label>
+                </li>
+              </ul>
+            </div>
 
-        );
-      });
+          );
+        });
+      }
     }
-  }
 
     return (
       <div>
