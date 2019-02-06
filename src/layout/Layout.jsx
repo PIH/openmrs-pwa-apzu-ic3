92545c4deb6183afc7b746eae54309c20587d921
jomadoye <omadoyejed@gmail.com>
2019-02-06 16:53:40 +0100

    Remove refrences to screening in react-component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { BasicLayout, SystemAlert } from '@openmrs/react-components';
 import logo from "../assets/images/pih_apzu_logo_white.png";
 import potraitLogo from '../assets/images/hands_logo.png';
@@ -7,16 +8,17 @@ import Footer from '../footer/Footer';
 import PatientAlert from '../patient/PatientAlert';
 import utils from '../utils';
 
-const Layout = props => {
-
+const Layout = (props) => {
   return (
     <div>
       <BasicLayout
+        PatientAlert={PatientAlert}
+        SystemAlert={SystemAlert}
+        backLink={props.lastScreeningQueue}
         identifiersToDisplay={utils.getIdentifiersToDisplay}
         logo={logo}
         navMenuPages={NAV_MENU_PAGES}
-        PatientAlert={PatientAlert}
-        SystemAlert={SystemAlert}
+        patientHeaderBacklink="/screening"
         smallWidthLogo={potraitLogo}
         userMenuPages={USER_MENU_PAGES}
         {...props}
@@ -26,5 +28,11 @@ const Layout = props => {
   );
 };
 
-export default Layout;
+const mapStateToProps = (state) => {
+  return {
+    lastScreeningQueue: state.screening.LAST_SCREENING_QUEUE
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
 
