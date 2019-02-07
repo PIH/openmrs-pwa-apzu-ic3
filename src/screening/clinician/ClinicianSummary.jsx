d2863317155fe82d9cf541502bd9dc385214e335
mgoodrich <mark.goodrich@live.com>
2019-02-07 13:54:05 -0500

    MLW-1028: Clinician Station summary not indicating obs as abnormal/critical

--- src/screening/clinician/ClinicianSummary.jsx
+++ src/screening/clinician/ClinicianSummary.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {ObsHistory, ProgramEnrollment, selectors, formUtil } from '@openmrs/react-components';
 import ChronicCareDiagnoses from './ChronicCareDiagnoses';
+import {CONCEPTS} from "../../constants";
 
 const ClinicianSummary = props => {
 
@@ -23,6 +24,42 @@ const ClinicianSummary = props => {
       <ChronicCareDiagnoses />
       <h4><u>Visit Summary</u></h4>
       <ObsHistory
+        concepts={[
+          CONCEPTS.Weight,
+          CONCEPTS.Height,
+          CONCEPTS.BMI,
+          CONCEPTS.Pregnant,
+          CONCEPTS.MUAC,
+          CONCEPTS.DiastolicBloodPressure,
+          CONCEPTS.SystolicBloodPressure,
+          CONCEPTS.ViralLoadResult,
+          CONCEPTS.ViralLoadLowerThanDetectionLimit,
+          CONCEPTS.Bled,
+          CONCEPTS.ReasonForNoSample,
+          CONCEPTS.ReasonForTesting,
+          // CONCEPTS.LabLocation,
+          CONCEPTS.HIV_TEST_TYPE,
+          CONCEPTS.SampleCollected,
+          CONCEPTS.ReasonForNoSample,
+          CONCEPTS.ReasonForTesting,
+          // CONCEPTS.LabLocation,
+          CONCEPTS.HIV_TEST_RESULTS,
+          CONCEPTS.TBTestType,
+          CONCEPTS.GeneXpert,
+          CONCEPTS.Smear,
+          CONCEPTS.RifampinResistance,
+          CONCEPTS.ReasonForNoResult,
+          // CONCEPTS.SampleCollected,
+          CONCEPTS.LabLocation,
+          CONCEPTS.SampleQuality,
+          CONCEPTS.TB.SymptomPresent,
+          CONCEPTS.TB.SymptomAbsent,
+          CONCEPTS.ADHERENCE_COUNSELING.AdherenceSession,
+          CONCEPTS.ADHERENCE_COUNSELING.NameOfCounselor,
+          CONCEPTS.ADHERENCE_COUNSELING.CounseledOnPillCounts,
+          CONCEPTS.ADHERENCE_COUNSELING.DrugAdherencePercentage,
+          CONCEPTS.ADHERENCE_COUNSELING.CounseledOnViralLoad
+        ]}
         obs={obs}
         showDates={false}
       />
