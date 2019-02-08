225311210c328f564574a88ff4f25b78aeca5489
rowland henshaw <rowland.henshaw@gmail.com>
2019-02-08 02:16:12 +0100

    removed irrelevvant test

--- src/screening/bloodPressure/__tests__/bloodPressureFilters.test.js
+++ src/screening/bloodPressure/__tests__/bloodPressureFilters.test.js
@@ -3,41 +3,6 @@ import { ENCOUNTER_TYPES } from "../../../constants";
 
 describe('bloodPressureFilters', () => {
 
-  it('required: should filter out non-adult patients', () => {
-
-    const results = [
-      {
-        uuid: 'abcd',
-        age: 20,
-        visit: {
-          encounters: [
-            {
-              encounterType: {
-                uuid: ENCOUNTER_TYPES.CheckInEncounterType.uuid
-              }
-            }
-          ]
-        }
-      },
-      {
-        uuid: '5678',
-        age: 10,
-        visit: {
-          encounters: [
-            {
-              encounterType: {
-                uuid: ENCOUNTER_TYPES.CheckInEncounterType.uuid
-              }
-            }
-          ]
-        }
-      },
-    ].filter(bloodPressureFilters.required);
-
-    expect(results.length).toBe(1);
-    expect(results[0].uuid).toBe("abcd");
-  });
-
   it('completed: should filter out patients without blood pressure encounter', () => {
 
     const results = [
