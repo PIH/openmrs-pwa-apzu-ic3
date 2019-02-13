7e77717def51053f09fc4f19c22bdf53a525e24a
mgoodrich <mark.goodrich@live.com>
2019-02-13 13:51:36 -0800

    comment out Search tests entirely

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,36 @@
+describe('Search', function () {
+
+  // TODO: these are failing (and will be failing) until the login tests are correted to properly log users out
+
+  /*  before(function () {
+      // runs once before all tests in the block, defined in commands.js
+      cy.login();
+    });
+
+
+    // TODO remove "skip" to enable test
+    it('Should search for patient by name', function () {
+      cy.visit('/');
+
+      // TODO why doesn't this work?
+     /!* cy.contains('Search Patient')
+        .click();*!/
+      // TODO
+    });
+
+    // TODO remove "skip" to enable test
+    it('Should search for patient by identifier', function () {
+      cy.visit('/');
+
+      // TODO why doesn't this work?
+      cy.contains('Search Patient')
+        .focus()
+        .click();
+      // TODO
+    });
+
+    after(function () {
+      cy.logout();
+    });*/
+
+});
