2c331398adae01933fab45cda44d5f460eda6424
rowland henshaw <rowland.henshaw@gmail.com>
2019-02-25 12:58:18 +0100

    MLW-264: completed nutrition tests

--- cypress/integration/nutrition.spec.js
+++ cypress/integration/nutrition.spec.js
@@ -81,10 +81,81 @@ describe('Nutrition form', function () {
         cy.get('.form-action-btns > button')
           .first()
           .click();
+      });
+  });
 
-        cy.get('.custom-loader')
+  it('save button should be disabled for invalid values for height and weight', function () {
+    cy.wait(3000);
+    cy.searchPatientByID('MGT-0148-CCC');
+
+    // Select the patient
+    cy.get('.card-list')
+      .first()
+      .click();
+
+    // Navigate to nutrition  summary
+    cy.get('[href="#/screening/nutrition/form"]')
+      .first()
+      .click();
+
+    // Navigate to nutrition form
+    cy.get('.summary-swiper-button')
+      .click();
+
+    cy.wait(3000);
+    // Check if form is in edit mode
+    cy.get('.form-action-btns > button')
+      .first()
+      .then(($button) => {
+        const text = $button.text();
+        // if form is in EDIT mode ...
+        if (text === 'Edit') {
+
+          // Put form in EDIT mode
+          cy.get('.form-action-btns > button')
+            .first()
+            .click();
+          cy.wait(2000); 
+
+          // Input value for height
+          cy.get('#formWeight')
+            .type('120');
+          
+          // Input value for height
+          cy.get('#formHeight')
+            .type('220');
+
+          // Input value for MUAC
+          cy.get('#formMuac')
+            .type('12.5');
+        }
+
+
+        // Input value for height
+        cy.get('#formWeight')
+          .type('120');
+        
+        // Input value for height
+        cy.get('#formHeight')
+          .type('220');
+
+        // Input value for MUAC
+        cy.get('#formMuac')
+          .type('12.5');
+
+        // assert that validations are shown when form values exceed valid limits
+        cy.get('#formWeight + div.div-error > span.field-error')
+          .contains('Must be less than 101')
+          .should('exist');
+
+        cy.get('#formHeight + div.div-error > span.field-error')
+          .contains('Must be less than 216')
+          .should('exist');
+
+        // asser that the save buttonn is disabled
+        cy.get('.form-action-btns > button')
           .first()
-          .should('be.visible');
+          .should('be.disabled');
       });
   });
 
