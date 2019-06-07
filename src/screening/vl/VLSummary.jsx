c8e083a8bdff5b7926e1c5ccd5c6f7b583d4a7bc
mgoodrich <mark.goodrich@live.com>
2019-06-07 16:40:07 -0400

    revert: IS-430: Can't edit encounters created outside of IC3 from within IC3

--- src/screening/vl/VLSummary.jsx
+++ src/screening/vl/VLSummary.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {CONCEPTS, ENCOUNTER_TYPES} from "../../constants";
-import {formActions, ObsHistory, formUtil} from "@openmrs/react-components";
+import {formActions, ObsHistory} from "@openmrs/react-components";
 
 const VLSummary = props => {
   return (
@@ -20,8 +20,6 @@ const VLSummary = props => {
             CONCEPTS.ViralLoadDetectablelowerLimit
           ]}
         editableEncounterTypes={[ENCOUNTER_TYPES.VLEncounterType]}
-        // hack: some of the VL encounters are entered via the flowsheets, and currently uneditable here because they don't include the form and path; need to make sure we don't show an edit link
-        editableFunc={(encounter, obs) => obs.some((o) => formUtil.hasFormAndPath(o))}
         groupingConcepts={[CONCEPTS.ViralLoadTestSet]}
         onEditEncounterActionCreators={[
           (encounterUuid) => formActions.loadFormBackingEncounter(props.formInstanceId, encounterUuid)
