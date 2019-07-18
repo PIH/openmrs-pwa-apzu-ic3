5a13e86bf67b1c9bb3d0f63ae7752d4796c2c6ad
Cosmin Ioan <cioan@pih.org>
2019-07-18 16:18:14 -0400

    IS-204, clear clinical notes and update screening concept display name

--- src/screening/cervical/CervicalCancerForm.jsx
+++ src/screening/cervical/CervicalCancerForm.jsx
@@ -12,11 +12,15 @@ class CervicalCancerForm extends React.PureComponent {
   componentDidUpdate(prevProps) {
     const {
       cervicalScreenningResults,
+      clinicalNotes,
       biopsyDone
 
     } = this.props;
 
     if (typeof cervicalScreenningResults.value !== 'undefined' && cervicalScreenningResults.value !== prevProps.cervicalScreenningResults.value) {
+      if (cervicalScreenningResults.value !== CONCEPTS.Other.uuid) {
+        this.clearField(clinicalNotes.fieldName);
+      }
       if (cervicalScreenningResults.value === CONCEPTS.VIA_NEGATIVE.uuid) {
         this.clearField(biopsyDone.fieldName);
       }
@@ -52,6 +56,7 @@ class CervicalCancerForm extends React.PureComponent {
                   concept={CONCEPTS.CERVICAL_CANCER_SCREENING_RESULTS.uuid}
                   conceptAnswers={FORM_ANSWERS.cervicalCancerResultAnswers}
                   path="cervical-cancer-results"
+                  required={ true }
                 />
               </FormGroup>
             </Col>
@@ -99,6 +104,7 @@ class CervicalCancerForm extends React.PureComponent {
                 concept={CONCEPTS.BIOPSY_DONE.uuid}
                 conceptAnswers={FORM_ANSWERS.trueFalse}
                 path="cervical-cancer-biopsy"
+                required={ (typeof cervicalScreenningResults.value !== 'undefined') && (cervicalScreenningResults.value !== CONCEPTS.VIA_NEGATIVE.uuid) ? true : false }
               />
             </FormGroup>
           </Col>
@@ -126,13 +132,17 @@ export default connect((state, props) => {
   const selector = formValueSelector(props.formInstanceId);
 
   const cervicalScreenningResultsFieldName = formUtil.obsFieldName(['cervical-cancer-screening-construct', 'cervical-cancer-results'], [CONCEPTS.CERVICAL_CANCER_SCREENING_CONSTRUCT.uuid, CONCEPTS.CERVICAL_CANCER_SCREENING_RESULTS.uuid]);
-
+  const clinicalNotesFieldName = formUtil.obsFieldName(['cervical-cancer-screening-construct', 'clinical-notes'], [CONCEPTS.CERVICAL_CANCER_SCREENING_CONSTRUCT.uuid, CONCEPTS.Clinical.ClinicalNotes.uuid]);
   const biopsyDone = formUtil.obsFieldName(['cervical-cancer-screening-construct', 'cervical-cancer-biopsy'], [CONCEPTS.CERVICAL_CANCER_SCREENING_CONSTRUCT.uuid, CONCEPTS.BIOPSY_DONE.uuid]);
 
   return {
     cervicalScreenningResults: {
       fieldName: cervicalScreenningResultsFieldName,
       value: selector(state, cervicalScreenningResultsFieldName)
+    },
+    clinicalNotes: {
+      fieldName: clinicalNotesFieldName,
+      value: selector(state, clinicalNotesFieldName)
     },
     biopsyDone: {
       fieldName: biopsyDone,
